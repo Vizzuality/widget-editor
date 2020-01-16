e8736e0429f34cc08c0addfd74d7e53894bae402
moose <tomasmoosee@gmail.com>
2020-01-16 10:53:45 +0100

    Updates

---
+++ precache-manifest.efcb9fea8fb1deaeb402d6292a2810e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61cbdb1b402172ff0ed8928f6dd9e19f",
+    "url": "/widget-editor/index.html"
+  },
+  {
+    "revision": "217bcbda4d9a4c8e788b",
+    "url": "/widget-editor/static/css/main.6f66ba81.chunk.css"
+  },
+  {
+    "revision": "18bad94c24f960388a42",
+    "url": "/widget-editor/static/js/2.a7ecd66c.chunk.js"
+  },
+  {
+    "revision": "217bcbda4d9a4c8e788b",
+    "url": "/widget-editor/static/js/main.89190fa0.chunk.js"
+  },
+  {
+    "revision": "8a2711e88c9f8fd01860",
+    "url": "/widget-editor/static/js/runtime-main.5fd221c7.js"
+  }
+]);
