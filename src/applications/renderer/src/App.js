59a8ef6e1b9d836876c7a5b00844e5c5acff1a2d
Tomas <tomasmoosee@gmail.com>
2020-02-18 10:52:38 +0100

    remove print test utility from renderer

--- src/applications/renderer/src/App.js
+++ src/applications/renderer/src/App.js
@@ -5,17 +5,11 @@ import "./App.css";
 import RwAdapter from "@packages/rw-adapter";
 
 function App() {
-  const testAdapter = new RwAdapter("Hello world");
-
-  console.log("test adapter", testAdapter.print());
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          App Renderer <code>{testAdapter.print()}</code> from RWAdapter.
-        </p>
+        <p>App Renderer from RWAdapter.</p>
       </header>
     </div>
   );
