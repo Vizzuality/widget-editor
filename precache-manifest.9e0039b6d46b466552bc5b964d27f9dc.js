7bf42943e04dff76111fc00810d33d520fde9687
Tomas <tomasmoosee@gmail.com>
2020-02-26 15:29:48 +0100

    Updates

---
+++ precache-manifest.9e0039b6d46b466552bc5b964d27f9dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f92ad627ffda7a784358ef10f4d6c9",
+    "url": "/widget-editor/index.html"
+  },
+  {
+    "revision": "c46b36e75c271193f295",
+    "url": "/widget-editor/static/css/main.c86a48ed.chunk.css"
+  },
+  {
+    "revision": "0744cb0c97d3ec461018",
+    "url": "/widget-editor/static/js/2.8709528b.chunk.js"
+  },
+  {
+    "revision": "c46b36e75c271193f295",
+    "url": "/widget-editor/static/js/main.aae64045.chunk.js"
+  },
+  {
+    "revision": "fad4bf9fb422e1d0ded4",
+    "url": "/widget-editor/static/js/runtime-main.da8670b2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/widget-editor/precache-manifest.6b8b74d00072d323ebce664f0c5141ff.js"
+  "/widget-editor/precache-manifest.9e0039b6d46b466552bc5b964d27f9dc.js"
 );
 
 self.addEventListener('message', (event) => {
