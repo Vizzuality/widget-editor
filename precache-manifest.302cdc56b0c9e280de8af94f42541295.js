0c53bac5e156ca36548bc2709770a18b90e5ebee
Tomas <tomasmoosee@gmail.com>
2020-04-28 12:00:31 +0200

    Updates

--- precache-manifest.302cdc56b0c9e280de8af94f42541295.js
+++ precache-manifest.302cdc56b0c9e280de8af94f42541295.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "947ba9377dcd3a94eaea946d1e258610",
+    "revision": "3876e28e5deb6587a62713feff1a22ff",
     "url": "/widget-editor/index.html"
   },
   {
-    "revision": "cfde260180fc1932845e",
-    "url": "/widget-editor/static/css/main.12dc8dad.chunk.css"
+    "revision": "2d77b0b4b05028e77503",
+    "url": "/widget-editor/static/css/main.e6694f22.chunk.css"
   },
   {
     "revision": "4b8928ee851e4aed6e13",
     "url": "/widget-editor/static/js/2.8ea1cf5c.chunk.js"
   },
   {
-    "revision": "cfde260180fc1932845e",
-    "url": "/widget-editor/static/js/main.4b0798a2.chunk.js"
+    "revision": "2d77b0b4b05028e77503",
+    "url": "/widget-editor/static/js/main.c244b6dc.chunk.js"
   },
   {
     "revision": "b084ad17df47e61f24c9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/widget-editor/precache-manifest.fbc793d59ce97dc1b49a539b3baa9793.js"
+  "/widget-editor/precache-manifest.302cdc56b0c9e280de8af94f42541295.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/widget-editor/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
