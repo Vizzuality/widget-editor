c1c240005debb31093db12e97e1d1ca5be7e7dde
Tomas <tomasmoosee@gmail.com>
2020-05-06 11:35:29 +0200

    fix issue for YEAR dates, they should not be parsed

--- src/packages/core/src/charts/helpers.ts
+++ src/packages/core/src/charts/helpers.ts
@@ -1,11 +1,16 @@
 import { MONTHS } from "../constants";
 
-export const parseData = data => {
-  return data ? data.map(d => {
-    if (Date.parse(d.x)) {
-      const date = new Date(d.x);
-      d.x = `${date.getDate()} ${MONTHS[date.getMonth()]}`;
-    }
-    return d;
-  }) : [];
-}
+export const parseData = (data) => {
+  return data
+    ? data.map((d) => {
+        if (Date.parse(d.x)) {
+          // XXX : If date is "year" just return the year
+          if (typeof d.x === "number" && d.x.toString().length !== 4) {
+            const date = new Date(d.x);
+            d.x = `${date.getDate()} ${MONTHS[date.getMonth()]}`;
+          }
+        }
+        return d;
+      })
+    : [];
+};
