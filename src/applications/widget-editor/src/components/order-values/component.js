f934461e9d7e4e18bb1a52dabfc6fd5cb85d2b09
Clément Prod'homme <contact@clementprodhomme.fr>
2020-06-08 10:25:30 +0100

    Display aliases for the Order by field of the UI

--- src/applications/widget-editor/src/components/order-values/component.js
+++ src/applications/widget-editor/src/components/order-values/component.js
@@ -30,13 +30,13 @@ const ORDER_TYPES = [
 
 const OrderValues = ({ orderBy, columns, setFilters, onChange }) => {
   const options = columns.map((c) => ({
-    value: c.name || c.alias || c.identifier,
-    label: c.name || c.alias || c.identifier,
+    value: c.name,
+    label: c.alias || c.name || c.identifier,
   }));
 
-  const selectedOption = options.find((o) =>
-    orderBy ? o.label === orderBy.name : null
-  );
+  const selectedOption = orderBy
+    ? options.find(option => option.value === orderBy.name)
+    : null;
 
   let selectedOrder;
   if (orderBy && orderBy.orderType) {
