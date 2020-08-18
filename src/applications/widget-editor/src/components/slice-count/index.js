8e0698fa717c20d8c1706f0870c210543af4db8b
Clément Prod'homme <contact@clementprodhomme.fr>
2020-08-18 17:00:21 +0100

    Add a note to the slice number setting

--- src/applications/widget-editor/src/components/slice-count/index.js
+++ src/applications/widget-editor/src/components/slice-count/index.js
@@ -1,3 +1,10 @@
+import { connectState } from "@widget-editor/shared/lib/helpers/redux";
+import { selectDisabledFeatures } from "@widget-editor/shared/lib/modules/editor/selectors";
+
 import DonutSlicesComponent from "./component";
 
-export default DonutSlicesComponent;
+export default connectState(
+  state => ({
+    disabledFeatures: selectDisabledFeatures(state),
+  }),
+)(DonutSlicesComponent);
