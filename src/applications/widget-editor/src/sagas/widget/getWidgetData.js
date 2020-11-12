f634e9cc8de1ad59cf4cfad01fbd3c0f38b82500
Tomas Eriksson <tomasmoosee@gmail.com>
2020-11-12 12:54:08 +0100

    remove chart specific colum cheecks, not needed

--- src/applications/widget-editor/src/sagas/widget/getWidgetData.js
+++ src/applications/widget-editor/src/sagas/widget/getWidgetData.js
@@ -3,20 +3,15 @@ import { getLocalCache } from "exposed-hooks";
 
 /**
  * @generator getWidgetDataWithAdapter
- * utilizing the specified adapter to request widget data
+ * utilising the specified adapter to request widget data
  * @triggers <void>
  */
 function* getWidgetDataWithAdapter(editorState) {
   const { adapter } = getLocalCache();
 
-  const columnsSet = (value, category, color, chartType) => {
+  const columnsSet = (value, category) => {
     const hasCategory = category && typeof category === "object" && "name" in category;
     const hasValue = value && typeof value === "object" && "name" in value;
-    const hasColor = color && typeof color === "object" && "name" in color;
-
-    if (chartType === 'donut' || chartType === 'pie') {
-      return hasValue && hasColor;
-    }
     return hasCategory && hasValue;
   };
 
