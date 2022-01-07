82c875245193ba4b210fadc2fe7d3ddc4d51e247
Tomas Eriksson <tomasmoosee@gmail.com>
2022-01-07 11:20:20 +0100

    update jest for m1 support

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,15 @@
 module.exports = {
-  roots: ["<rootDir>/src"],
-  setupFiles: ["dotenv/config"],
+  roots: [
+    '<rootDir>/src',
+  ],
+  setupFiles: [
+    'dotenv/config',
+  ],
   testMatch: [
-    "**/__tests__/**/*test.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+    '**/?(*.)+(spec|test).+(ts|tsx|js)',
+    '**/__tests__/**/*test.+(ts|tsx|js)',
   ],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest"
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   }
-};
+}
