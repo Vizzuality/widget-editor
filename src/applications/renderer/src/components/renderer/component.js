89435b59ca8cb0c6a485c734b2397754b8619022
Tomas Eriksson <tomasmoosee@gmail.com>
2022-01-27 12:51:21 +0100

    style no data view for renderer

--- src/applications/renderer/src/components/renderer/component.js
+++ src/applications/renderer/src/components/renderer/component.js
@@ -3,11 +3,21 @@ import PropTypes from 'prop-types';
 import has from 'lodash/has';
 import { getDefaultTheme } from "@widget-editor/core";
 import Map from "@widget-editor/map";
+import styled from 'styled-components';
 
 import Legend from "components/legend";
 import Chart from "components/chart";
 import useLayerData from "./fetch-layers-hook";
 import useWidgetData from "./fetch-data-hook";
+
+const StyledNoData = styled.p`
+  display: flex;
+  width: 100%;
+  font-size: 16px;
+  align-items: center;
+  justify-content: center;
+  color: rgb(189, 189, 189);
+`;
 
 const Renderer = ({
   adapter,
@@ -19,7 +29,7 @@ const Renderer = ({
   if (typeof adapter !== "function") {
     throw new Error("Renderer: Missing prop adapter and adapter needs to be of type Adapter");
   }
-
+  
   const adapterInstance = useMemo(() => new adapter(), [adapter]);
 
   const isMap = widgetConfig?.paramsConfig?.visualizationType === "map";
@@ -57,7 +67,7 @@ const Renderer = ({
     && chartWidgetConfig?.legend?.length > 0
     && !thumbnail;
 
-  const hasNoData = dataURL && widgetData?.length === 0;
+  const hasNoData = dataURL && !widgetData || widgetData?.length === 0;
 
   if (isLoadingLayers) {
     return "Loading...";
@@ -75,9 +85,8 @@ const Renderer = ({
           {!hasNoData && (
             <Chart widgetConfig={chartWidgetConfig} thumbnail={thumbnail} />
           )}
-          {/* TODO: Add styles for no data */}
           {hasNoData && (
-            <p>No data present</p>
+            <StyledNoData>No data available</StyledNoData>
           )}
         </Suspense>
       )}
